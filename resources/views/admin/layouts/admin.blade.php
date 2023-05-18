9e25b7f95b4b68406883fb02515f6dbd2a7c6d6a
mwangangimusyoka1998@gmail.com <mwangangimusyoka1998@gmail.com>
2023-05-19 01:01:48 +0300

    ✨feature: admin customization to cafe

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -21,7 +21,7 @@
         {{-- sidebar nav --}}
         <aside :class="{ '-translate-x-full': !open }"
             class="z-10 bg-blue-800 text-white w-64 px-2 py-4 absolute inset-y-0 left-0 md:relative transform md:translate-x-0
-     overflow-y-auto transition ease-in-out dur shadow-lg">
+     overflow-y-auto transition ease-in-out duration shadow-lg">
             <div class="flex items-center justify-between px-2">
                 {{-- logo --}}
                 <div class="flex items-center px-2 space-x-2">
@@ -47,18 +47,127 @@
                 <div class="nav-links">
                     <ul class="space-y-2">
                         <li>
-                          <a href="#" class="block py-2 px-4">Home</a>
+                            <span><i class=""></i></span> <a href="#" class="block py-2 px-4">Dashboard</a>
+
+
+                        <li class="px-4 py-2">
+                            <div class="flex flex-row items-center h-8">
+                                <div class="text-sm font-light tracking-wide text-white-500 capitalize">User Management
+                                </div>
+                            </div>
                         </li>
                         <li>
-                          <a href="#" class="block py-2 px-4">About</a>
+                            <a href="#"
+                                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-white-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                                <span class="inline-flex justify-center items-center ml-4">
+                                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z">
+                                        </path>
+                                    </svg>
+                                </span>
+                                <span class="ml-2 text-sm tracking-wide truncate ">
+                                    Add user
+                                </span>
+                            </a>
                         </li>
                         <li>
-                          <a href="#" class="block py-2 px-4">Services</a>
+                            <a href="#"
+                                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-white-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                                <span class="inline-flex justify-center items-center ml-4">
+                                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z">
+                                        </path>
+                                    </svg>
+                                </span>
+                                <span class="ml-2 text-sm tracking-wide truncate ">
+                                    View Users
+                                </span>
+                            </a>
                         </li>
                         <li>
-                          <a href="#" class="block py-2 px-4">Contact</a>
+                            <a href="#"
+                                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-white-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                                <span class="inline-flex justify-center items-center ml-4">
+                                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z">
+                                        </path>
+                                    </svg>
+                                </span>
+                                <span class="ml-2 text-sm tracking-wide truncate ">
+                                    Add user
+                                </span>
+                            </a>
                         </li>
-                      </ul>
+
+
+                        <li class="px-5">
+                            <div class="flex flex-row items-center h-8">
+                                <div class="text-sm font-light tracking-wide text-white-500">Reports</div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-white-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                                <span class="inline-flex justify-center items-center ml-4">
+                                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01">
+                                        </path>
+                                    </svg>
+                                </span>
+                                <span class="ml-2 text-sm tracking-wide truncate">Total sales</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-white-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                                <span class="inline-flex justify-center items-center ml-4">
+                                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01">
+                                        </path>
+                                    </svg>
+                                </span>
+                                <span class="ml-2 text-sm tracking-wide truncate">Total per cashier</span>
+                            </a>
+                        </li>
+
+                        <button
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
+                        type="button" data-dropdown-toggle="dropdown">{{ Auth::user()->name }} <svg class="w-4 h-4 ml-2"
+                            fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M19 9l-7 7-7-7"></path>
+                        </svg></button>
+                    <!-- Dropdown menu -->
+                    <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4"
+                        id="dropdown">
+                        <div class="px-4 py-3">
+                            <span class="block text-sm"></span>
+                            <span class="block text-sm font-medium text-gray-900 truncate">{{ Auth::user()->name }}</span>
+                        </div>
+                        <ul class="py-1" aria-labelledby="dropdown">
+                                                     <li>
+                                <a href="#"
+                                    class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Settings</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Sign out</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </ul>
                 </div>
             </nav>
         </aside>
